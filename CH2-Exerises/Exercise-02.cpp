007e250d573e914df6036858b68f0377e77a754c
HeinzZaw <152692669+Heinzaw215@users.noreply.github.com>
2024-07-01 19:43:13 +0630

    Add files via upload

---
+++ CH2-Exerises/Exercise-02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+int main()
+{
+    // use setw() function to set the width of the output field
+    cout << "1990" << setw(8) << "135" << endl;
+    cout << "1991" << setw(8) << "7290" << endl;
+    cout << "1992" << setw(8) << "11300" << endl;
+    cout << "1993" << setw(8) << "16200" << endl;
+    return 0;
+}
