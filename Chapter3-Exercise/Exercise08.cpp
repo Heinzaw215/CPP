3782322211704eea8e5458e7be290c12deeeae50
HeinzZaw <152692669+Heinzaw215@users.noreply.github.com>
2024-07-25 00:28:09 +0630

    Update Exercise08.cpp

--- Chapter3-Exercise/Exercise08.cpp
+++ Chapter3-Exercise/Exercise08.cpp
@@ -3,35 +3,29 @@ using namespace std;
 
 int main()
 {
-    /*
-    1 pound = 20 shilling
-    1 shilling = 12 pence;
-    */
-
     // f = first, s = second, t = total
     int fPound, fShilling, fPence;
     int sPound, sShilling, sPence;
     int tPound, tShilling, tPence;
-    char respond = 'y';
-    char dummychar;
+    char respond, dummychar;
 
     do {
-        cout << "Enter the first amount: " << '\x9c';
+        cout << "Enter the first amount (Pound.Shilling.Pence): " << '\x9c';
         cin >> fPound >> dummychar >> fShilling >> dummychar >> fPence;
-        cout << "Enter the second amount: " << '\x9c';
+        cout << "Enter the second amount (Pound.Shilling.Pence): " << '\x9c';
         cin >> sPound >> dummychar >> sShilling >> dummychar >> sPence;
 
         tPence = fPence + sPence;
         tShilling = fShilling + sShilling;
         tPound = fPound + sPound;
 
-        if (tPence > 11)
+        if (tPence >= 12)
         {
             tPence -= 12;
             tShilling += 1;
         }
 
-        if (tShilling > 19)
+        if (tShilling >= 20)
         {
             tShilling -= 20;
             tPound += 1;
@@ -39,8 +33,8 @@ int main()
 
         cout << "Total = \x9c" << tPound << "." << tShilling << "." << tPence << endl;
 
-        cout << "Do you wish to continue (y|n): ";
+        cout << "Do you wish to continue? (y | n): ";
         cin >> respond;
-    } while (respond != 'n');
+    } while (respond == 'y');
     return 0;
 }
